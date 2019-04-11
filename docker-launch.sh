2edb9916ff0c83ae36f2ea61cb1ff5f620d9edb1
Matti Karjalainen <matti.karjalainen@solita.fi>
2019-04-11 15:48:16 +0300

    env variables added. image changed

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v ~/.aws:/root/.aws dtjohnson/aws-azure-login "$@"
+docker run -e "AUTHENTICATOR_KEY=${AUTHENTICATOR_KEY}" \
+    -e "AZURE_DEFAULT_ROLE_ARN=${AZURE_DEFAULT_ROLE_ARN}" \
+    -e "AZURE_TENANT_ID=${AZURE_TENANT_ID}" \
+    -e "AZURE_APP_ID_URI=${AZURE_APP_ID_URI}" \
+    -e "AZURE_DEFAULT_USERNAME=${AZURE_DEFAULT_USERNAME}" \
+    -e "AZURE_DEFAULT_ROLE_ARN=${AZURE_DEFAULT_ROLE_ARN}" \
+    -e "AZURE_DEFAULT_DURATION_HOURS=${AZURE_DEFAULT_DURATION_HOURS}" \
+    --rm -it -v ~/.aws:/root/.aws kungfo0/aws-azure-login "$@"
